85cfb2eb9d0744ed612cf74495ae44f6ddd4e2ff
mvf4z7 <mike.fanger@gmail.com>
2015-04-27 23:48:53 -0500

    Removed some of the dummy inserts that were used for testing when developing the database

--- database/openbook.sql
+++ database/openbook.sql
@@ -131,8 +131,6 @@ INSERT INTO Teaches VALUES('CS 1510', 'Angel Morales');
 INSERT INTO Teaches VALUES('CS 3500', 'Angel Morales');
 INSERT INTO Teaches VALUES('CS 1510', 'Dan Lin');
 
-INSERT INTO Document VALUES(33, 'doc33', '2015-06-17', 'Angel Morales', 'CS 1510', 0, 'Fall', '2009', 'A', 99, NULL);
-INSERT INTO Document VALUES(24, 'another doc', '2015-06-17', 'Fikret Ercal', 'CS 3800', 0, 'Spring', '2001', 'A', 78, NULL);
 
 
 
